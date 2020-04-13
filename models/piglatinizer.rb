fc93a406c63f0f4efdbfcfbb6064b491d71e6422
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-04-13 03:20:54 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ def piglatinize(word) #pig latinizes a single word
    else
     vowel_index = word.index(/[aeiou]/)
     front_end = word.slice!(0..vowel_index-1)
-   front_end+ word+"ay"
+    word + front_end +"ay"
   end
 end
  
